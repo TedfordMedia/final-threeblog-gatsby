c1da98ef96bf191eeb36a621efe5858d4d468590
TedfordMedia <ted@tedfordmedia.com>
2021-04-21 01:57:42 +0100

    updates of template data

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -16,23 +16,20 @@ export default ({ data }) => {
       <Layout>
         <h1>About <span className="brand-diamond">◆</span></h1>
         <div className={aboutStyles.grid}>
-          <Img className={aboutStyles.image} fluid={data.fileName.childImageSharp.fluid} alt="Picture of Stefan Devai" />
+          <Img className={aboutStyles.image} fluid={data.fileName.childImageSharp.fluid} alt="Picture of Ted" />
           <div className={aboutStyles.rectangle}></div>
           <div className={aboutStyles.lineTop}></div>
           <div className={aboutStyles.textWrapper}>
             <div className={aboutStyles.text}>
-              <h2>Hi, I'm Stefan Devai :)</h2>
-              <p>I'm a History student at the Sorbonne University in Paris and a Computer Science enthusiast. In this blog I can share some of my programming experiments and historical essays I've written.</p>
-              <p>I actively use four languages for different aspects of my life, so you will find here posts in Spanish, Portuguese, English and French.</p>
-              <p>I use <a href="https://wiki.archlinux.org/index.php/Arch_Linux" target="_blank" rel="noopener noreferrer">Arch Linux</a> as my main operating system, <a href="https://i3wm.org/" target="_blank" rel="noopener noreferrer">i3wm</a> as my window manager and both <a href="https://neovim.io/" target="_blank" rel="noopener noreferrer">NeoVim</a> / <a href="https://www.gnu.org/software/emacs/" target="_blank" rel="noopener noreferrer">GNU Emacs</a> as my text editors depending on the programming language.</p>
+              <h2>Hi, I'm Ted of TedfordMedia</h2>
+              <p>Coming soon! </p> 
             </div>
           </div>
           <div className={aboutStyles.lineBottom}></div>
 
           <div className={aboutStyles.socialIcons}>
-            <a href="https://github.com/stefandevai" target="_blank" rel="noopener noreferrer" aria-label="GitHub link"><GitHubIcon /></a>
-            <a href="https://instagram.com/stedevai" target="_blank" rel="noopener noreferrer" aria-label="Instagram link"><InstagramIcon /></a>
-            <a href="mailto:stefandevai@posteo.me" aria-label="Email Link"><EmailIcon /></a>
+            <a href="https://github.com/tedfordmedia" target="_blank" rel="noopener noreferrer" aria-label="GitHub link"><GitHubIcon /></a> 
+            <a href="mailto:ted@tedfordmedia.com" aria-label="Email Link"><EmailIcon /></a>
           </div>
         </div>
       </Layout>
